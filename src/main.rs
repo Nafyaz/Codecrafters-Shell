f872303e3b120b7b5af2648eb3053f5b52386824
Nafis Faiyaz <nafis.faiyaz.5@gmail.com>
2026-02-19 19:01:20 +0600

    Handle invalid commands

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,11 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input= String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read");
+
+    let command = input.trim();
+
+    print!("{command}: command not found")
 }
