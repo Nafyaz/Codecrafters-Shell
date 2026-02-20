e5fb9e8c2e74d3c5fd2e9fd5610f6a3cd8ca1192
Nafis Faiyaz <nafis.faiyaz.5@gmail.com>
2026-02-20 15:35:17 +0600

    Implement type

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,23 @@ fn main() {
         print!("$ ");
         io::stdout().flush().expect("Failed to flush stdout");
 
-        let mut input= String::new();
+        let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Failed to read");
 
-        let command = input.trim();
+        let args = input.trim().split_whitespace().collect::<Vec<&str>>();
 
-        if command == "exit" {
-            break;
-        }
-
-        let x = command.split(" ").collect::<Vec<&str>>();
-        if Some(&"echo") == x.get(0) {
-            println!("{}", &x[1..].join(" "));
+        if args.is_empty() {
             continue;
         }
 
-        println!("{command}: command not found");
+        match args.as_slice() {
+            ["exit", ..] => break,
+            ["echo", msg @ ..] => println!("{}", msg.join(" ")),
+            ["type", cmd] if ["exit", "echo", "type"].contains(cmd) => {
+                println!("{} is a shell builtin", cmd)
+            }
+            ["type", cmd @ ..] => println!("{}: not found", cmd.join(" ")),
+            _ => eprintln!("{}: command not found", args[0]),
+        }
     }
 }
